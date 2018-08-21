30e6fa54617ec607333155b534d1a9cb37e89752
Emil <emilburaczynski@gmail.com>
2018-08-21 19:20:28 +0200

    zmiany api

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', e => {
-
-    const searchTerm = searchInput.value; 
-
+    const searchTerm = searchInput.value;
     const sortBy = document.querySelector('input[name="sortBy"]:checked').value;
 
-    console.log(sortBy);
+    const searchLimit = document.getElementById('limit').value;
 
+    console.log(searchLimit);
 
+    if (searchTerm === '') {
 
+        showMessage('Please add a seach term', 'alert-danger');
+
+    }
     e.preventDefault();
 })
+
+
+function showMessage(msg, className) {
+    const div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(msg));
+
+    const searchcontainer = document.getElementById('search-container');
+
+    const search = document.getElementById('search');
+
+    searchcontainer.insertBefore(div, search);
+
+    setTimeout(() => document.querySelector('.alert-danger').remove(), 2000)
+}
